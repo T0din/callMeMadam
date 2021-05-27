ba46ebc15487eeee950c0991fbc3348604244cb1
vinarnick <pvinarnick@extia.fr>
2021-05-27 10:58:51 +0200

    first attempt to TS

---
+++ my-app/src/boardGame/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const BoardGame = () => {
+  return (
+    <div
+      style={{
+        backgroundImage: `url("https://images.pexels.com/photos/355802/pexels-photo-355802.jpeg?cs=srgb&dl=pexels-pixabay-355802.jpg&fm=jpg")`,
+        width: "100%",
+        minHeight: "100vh",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "white",
+          width = "350px",
+          height: "80vh",
+          border: "1px ",
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default BoardGame;
